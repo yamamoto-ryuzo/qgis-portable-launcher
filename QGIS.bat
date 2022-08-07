66abcf4e9d563237fb4d3852fdb11737c056fd1d
yamamoto-ryuzo <ryu@yamakun.net>
2022-08-07 16:52:42 +0900

    サーバー・クライアントモード及びＬＧＷＡＮに対応

--- QGIS.bat
+++ QGIS.bat
@@ -1,65 +1,66 @@
 chcp 65001
-rem"↑　文字コードの指定　UTF-8＝65001"
-rem"見本後は必ず「””」で囲む"
-rem"====================環境設定===================="
-rem"起動フォルダをカレントフォルダに設定"
+rem "↑　文字コードの指定　UTF-8＝65001"
+rem "見本後は必ず「””」で囲む"
+rem "====================環境設定===================="
+rem "起動フォルダをカレントフォルダに設定"
 cd /d %~dp0
 
-rem"====================配信用QGISのインストール===================="
-rem"==========環境変数の設定=========="
-rem"外部環境変数の読込"
-rem"QGIS.cfg"の読み込み"
-for /f "tokens=1,* delims==" %%a in (QGIS.cfg) do (
-    set %%a=%%b
-)
+rem "====================配信用QGISのインストール===================="
+rem "==========環境変数の設定=========="
+rem "QGIS.cfg.bat"の読み込み"
+REM 設定ファイルの読み込み（結合）
+CALL QGIS.cfg.bat
 
-rem"ダウンロードサーバーの設定"
-set QGIS_download=%USERPROFILE%\Downloads\%QGIS_ver%
+rem "ダウンロードサーバーの設定"
+set QGIS_download=%QGIS_delivery%\%QGIS_ver%
 
-rem"保存先ホルダ名の設定"
-rem"部分参照は %str:~-m,-n% 後ろからm文字目から、最後のn文字分を除いたもの"
+rem "インストール先のフォルダ名の設定"
+rem "部分参照は %str:~-m,-n% 後ろからm文字目から、最後のn文字分を除いたもの"
 SET QGIS_Folder=%QGIS_ver:~-0,-4%
 
-rem"==========ファイルダウンロード=========="
-rem"インストール済みかどうかを確認"
-if exist %QGIS_Install%\QGIS_portable\%QGIS_Folder% (
-    goto Launch_QGIS
+rem "==========サーバーモード=========="
+if exist "QGIS_delivery_server.cfg" (
+    if exist "QGIS_internet.cfg" (
+        rem "配信ファイルがない場合は，配信ファイルのダウンロード"
+        if not exist %QGIS_download% (
+            rem "=====ファイルダウンロード"
+            rem "bitsadmin /transfer ＜ジョブ名＞ ＜URL＞ ＜保存先ファイル名＞"
+            bitsadmin /transfer download-%QGIS_Folder% %QGIS_http%%QGIS_ver%   %QGIS_download%
+        )
+    )
 )
 
-rem"=====ファイルダウンロード"
-rem"bitsadmin /transfer ＜ジョブ名＞ ＜URL＞ ＜保存先ファイル名＞"
-bitsadmin /transfer download-%QGIS_Folder% %QGIS_http%%QGIS_ver%   %QGIS_download%
-
-rem"=====Zipファイルの解凍 PowerShellコマンド"
-rem"Expand-Archive -Path ＜ZIPファイル＞　＜展開先フォルダ＞"
-powershell Expand-Archive -Path %QGIS_download% %QGIS_Install%\QGIS_portable
-
-
-rem"==========QGIS起動=========="
-:Launch_QGIS
-
-rem"QGIS_http=http://kouapp.main.jp/qgisp/"
-%QGIS_Install%\QGIS_portable\%QGIS_Folder%\qgis_p起動.bat
-
-rem"バッチ終了"
+rem "==========クライアントモード=========="
+if exist "QGIS_client.cfg" (
+    rem "=====QGIS起動"
+    rem "インストール済みかどうかを確認"
+    if not exist %QGIS_Install%\%QGIS_Folder% (
+        rem "=====Zipファイルの解凍 PowerShellコマンド"
+        rem "Expand-Archive -Path ＜ZIPファイル＞　＜展開先フォルダ＞"
+        powershell Expand-Archive -Path %QGIS_download% %QGIS_delivery%
+    )
+    rem "QGIS_http=http://kouapp.main.jp/qgisp/"
+    %QGIS_Install%\%QGIS_Folder%\qgis_p起動.bat
+)
+rem "バッチ終了"
 exit /b
 
 
-rem"====================関数設定===================="
-rem"関数STRLEN設定"
+rem "====================関数設定===================="
+rem "関数STRLEN設定"
 :STRLEN
 set str=%1
 set len=0
-rem"ループの先頭"
+rem "ループの先頭"
 :LOOP
-rem"文字列「str」が空じゃなければ"
+rem "文字列「str」が空じゃなければ"
 if not "%str%"=="" (
-    rem"文字列「str」から1文字削る"
+    rem "文字列「str」から1文字削る"
     set str=%str:~1%
-    rem"文字数「Len」を1カウントアップ"
+    rem "文字数「Len」を1カウントアップ"
     set /a len=%len%+1
-    rem"ループの先頭に戻る"
+    rem "ループの先頭に戻る"
     goto :LOOP
 )
-rem"バッチ終了"
+rem "バッチ終了"
 exit /b
---
+++ QGIS.cfg.bat
@@ -0,0 +1,10 @@
+rem "環境変数の設定"
+rem "ダウンロードサイト"
+set QGIS_http=http://kouapp.main.jp/qgisp/
+rem "ダウンロードファイル"
+set QGIS_ver=QGIS322_portable.zip
+rem "下記フォルダにはアクセス権必要"
+rem "下記フォルダ内に複数フォルダが生成される"
+set QGIS_Install=c:\QGIS_portable
+set QGIS_delivery=c:\QGIS_portable
+rem "set QGIS_delivery=%USERPROFILE%\Downloads"
