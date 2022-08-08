dcf24881517e4cf4da67195781d105bb4c3a70fb
Yamamoto Ryuzou <ryu@yamakun.net>
2022-08-08 20:03:36 +0900

    pigreco 本家　導入完了

--- QGIS.bat
+++ QGIS.bat
@@ -11,40 +11,51 @@ rem "QGIS.cfg.bat"の読み込み"
 REM 設定ファイルの読み込み（結合）
 CALL QGIS.cfg.bat
 
-rem "ダウンロードサーバーの設定"
-set QGIS_download=%QGIS_delivery%\%QGIS_ver%
-
-rem "インストール先のフォルダ名の設定"
-rem "部分参照は %str:~-m,-n% 後ろからm文字目から、最後のn文字分を除いたもの"
-SET QGIS_Folder=%QGIS_ver:~-0,-4%
+SET QGIS_Folder=QGIS_%QGIS_ver%
 
 rem "==========サーバーモード=========="
 if exist "QGIS_delivery_server.cfg" (
     if exist "QGIS_internet.cfg" (
         rem "配信ファイルがない場合は，配信ファイルのダウンロード"
-        if not exist %QGIS_download% (
+        if not exist %QGIS_delivery%\%QGIS_File% (
             rem "=====ファイルダウンロード"
             rem "bitsadmin /transfer ＜ジョブ名＞ ＜URL＞ ＜保存先ファイル名＞"
-            bitsadmin /transfer download-%QGIS_Folder% %QGIS_http%%QGIS_ver%   %QGIS_download%
+            bitsadmin /transfer download_QGIS_%QGIS_ver% %QGIS_http% %QGIS_delivery%
         )
     )
 )
 
 rem "==========クライアントモード=========="
 if exist "QGIS_client.cfg" (
-    rem "=====QGIS起動"
     rem "インストール済みかどうかを確認"
+    rem "インストール先のフォルダ名の設定"
     if not exist %QGIS_Install%\%QGIS_Folder% (
-        rem "=====Zipファイルの解凍 PowerShellコマンド"
-        rem "Expand-Archive -Path ＜ZIPファイル＞　＜展開先フォルダ＞"
-        powershell Expand-Archive -Path %QGIS_download% %QGIS_delivery%
+        if %site% == 1 (
+            rem "7z解凍"
+            7z.exe x -y %QGIS_delivery%\%QGIS_File% -o%QGIS_Install%
+        ) else (
+            rem "=====Zipファイルの解凍 PowerShellコマンド"
+            rem "Expand-Archive -Path ＜ZIPファイル＞　＜展開先フォルダ＞"
+            powershell Expand-Archive -Path  %QGIS_delivery%\%QGIS_File% %QGIS_Install%
+        )
+        cd %QGIS_Install%
+        rename %QGIS_extract_folder% %QGIS_Folder% 
+        rem "起動フォルダをカレントフォルダに設定"
+        cd /d %~dp0
     )
-    rem "QGIS_http=http://kouapp.main.jp/qgisp/"
-    %QGIS_Install%\%QGIS_Folder%\qgis_p起動.bat
-)
-rem "バッチ終了"
-exit /b
 
+    rem "QGISの起動"
+    rem "起動用フォルダに移動"
+    cd %QGIS_Install%\%QGIS_Folder%
+    if %site% == 1 (
+        %QGIS_Install%\%QGIS_Folder%\qgis-ltr-grass.bat
+    ) else (
+        %QGIS_Install%\%QGIS_Folder%\qgis_p起動.bat
+    )
+)
+
+rem "コマンドプロンプト終了"
+exit
 
 rem "====================関数設定===================="
 rem "関数STRLEN設定"
--- QGIS.cfg.bat
+++ QGIS.cfg.bat
@@ -1,10 +1,29 @@
-rem "環境変数の設定"
+rem "ダウンロードサイトの選択"
+rem "site="1" pigreco 本家"
+rem "site="2" kouapp　喜多さんのサイト"
+set site=1
+
 rem "ダウンロードサイト"
-set QGIS_http=http://kouapp.main.jp/qgisp/
-rem "ダウンロードファイル"
-set QGIS_ver=QGIS322_portable.zip
+if %site% == 1 (
+    rem バージョンは空白が入らないように””で囲むこと！
+    set QGIS_ver="3.22.9"
+    set QGIS_http=https://doc-00-1o-docs.googleusercontent.com/docs/securesc/60v1atgd7bougp4s2j4ke6u93vpnueb3/27ik8ft755983s5l1pvj7p9djcnkkj51/1659876900000/02571389660706551936/06654353961832540270/1-S272QXknZdWDbYcI2R9QBWo9pSNlUGI?e=download&ax=AI9vYm6eLelZJ6YdPAvdmzltQr7SwGdxM9SL0nhZjLZf2LEEMQhF3lqdMfHHcRtPZGKCR0Ye9WdHpCOOn3HcX680zXBEYFn2-nOoFv5jf3P_MzeyZLNSxhzBHikzfZvZHH65nW0u6Dh6V1ppR9_9RC6v92-UDa4VuZ6kN-HeEg2aDV4mjbUpYJzYXypK7tb50F840NnZBN3TEw6n2-gUEz27PgI7aAfJmvQZ47rnBAih4vXzJcTo4Xfe78z0GYvaSdeVTnCUY6rCRWQX-rze09Ba7RtHAw90ltkf8VY1VfFv8Ewyq_svMf2iq6x7Gn7nEK_-rt5J0tMlzqv17XhvqzGl2YBaHmgYm0wMXhUqG31E5FiLpDMCcX-Rb_69p3C4AEjuT-DRVwMu0alivg6gDqmI5ZYmpf88EDO2jVDXvUT_cX5Ed7rzlLDU7BbkW7Qn14gsdbuW8ZpGlAswuecvBeEZAllP8x5g1CmGBqRDimJr3QM3CU4Mb1IxJODwrG7MzT8iB6adAdKYOPOmqDjbLT7Cr2vgzghIbG58nNBlespynihpjP6uEVuZHbzdV3rAOWy4UDeWT-0X8Durm_-iRrmHKW2HYmw68dxTTjKxem8TQSetFaui-72sBxKaTUBtuuW8NZeKFfA4dVP9VjWymDnIYPBKiP4BQaj2W0GbvQbfXv4xgvI7Wj-YwnrClkpZ-tGtdsT8e-k2OQsMvb2g&uuid=1ced175a-0f9a-49d5-a1b1-f98e79b6a36b&authuser=0&nonce=f06nit0ppti08&user=06654353961832540270&hash=i24s02a221sfqoi1dacaa2d5699ktkjg
+    set QGIS_File=OSGeo4W64_3.22.9-ltr_grass-saga.7z
+    rem "解凍フォルダ"
+    set QGIS_extract_folder=OSGeo4W64
+) else (
+    rem バージョンは空白が入らないように””で囲むこと！
+    set QGIS_ver="3.22.6"
+    set QGIS_http=http://kouapp.main.jp/qgisp/QGIS322_portable.zip
+    set QGIS_File=QGIS322_portable.zip
+    rem "解凍フォルダ"
+    set QGIS_extract_folder=QGIS322_portable
+)
+
 rem "下記フォルダにはアクセス権必要"
-rem "下記フォルダ内に複数フォルダが生成される"
-set QGIS_Install=c:\QGIS_portable
+rem "配信用フォルダ"
 set QGIS_delivery=c:\QGIS_portable
+
+rem "インストール・フォルダ"
+set QGIS_Install=c:\QGIS_portable
 rem "set QGIS_delivery=%USERPROFILE%\Downloads"
